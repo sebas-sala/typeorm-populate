0683ee91c821ab1de7935bc5cb106e9262169cf0
sebas-sala <sebastian_orko@hotmail.com>
2024-10-14 00:30:46 -0600

    Refactor Factory class to use TypeORM repository and improve createOne and createMany methods

--- src/factory.ts
+++ src/factory.ts
@@ -1,14 +1,23 @@
-export abstract class Factory<Entity> {
-	async createOne(data?: any): Promise<Entity> {
-		throw new Error("Method not implemented.");
+import type { DataSource, ObjectLiteral, Repository } from "typeorm";
+
+export abstract class Factory<Entity extends ObjectLiteral> {
+	protected repository: Repository<Entity>;
+
+	constructor(dataSource: DataSource, entity: new () => Entity) {
+		this.repository = dataSource.getRepository(entity);
 	}
 
-	async createMany(count: number, data?: any): Promise<Entity[]> {
-		const list = [] as Entity[];
+	async createOne(data?: unknown): Promise<Entity> {
+		const entity = this.repository.create(data as Entity);
+		return this.repository.save(entity);
+	}
+
+	async createMany(count: number): Promise<Entity[]> {
+		const entities = [] as Entity[];
 		for (let i = 0; i < count; i++) {
-			const entity = await this.createOne(data);
-			list.push(entity);
+			const entity = await this.createOne();
+			entities.push(entity);
 		}
-		return list;
+		return entities;
 	}
 }
