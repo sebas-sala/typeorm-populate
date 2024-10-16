b28362a9bed547df0742b8299810e5598bbb3a04
sebas-sala <sebastian_orko@hotmail.com>
2024-10-16 00:27:09 -0600

    Add factories and seed file for database seeding

---
+++ samples/database/factories.ts
@@ -0,0 +1,7 @@
+import { UserFactory } from "@/test/factories/user.factory";
+import { PostFactory } from "@/test/factories/post.factory";
+
+export const factories = {
+	user: UserFactory,
+	post: PostFactory,
+};
