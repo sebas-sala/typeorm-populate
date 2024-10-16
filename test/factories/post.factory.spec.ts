332d36383d61d0abc911ca44c0d3a2ef45cbfb84
sebas-sala <sebastian_orko@hotmail.com>
2024-10-16 00:02:00 -0600

    Refactor postFactory tests and add data parameter support

--- test/factories/post.factory.spec.ts
+++ test/factories/post.factory.spec.ts
@@ -25,21 +25,74 @@ describe("PostFactory", () => {
 		await dataSource.destroy();
 	});
 
-	it("should create one post", async () => {
-		const post = await postFactory.createOne();
+	describe("createOne", () => {
+		it("should create one post", async () => {
+			const post = await postFactory.createOne();
 
-		expect(post.id).toBeDefined();
-		expect(post).toBeInstanceOf(Post);
-	});
-
-	it("should create many posts", async () => {
-		const posts = await postFactory.createMany(5);
-
-		expect(posts).toHaveLength(5);
-
-		for (const post of posts) {
 			expect(post.id).toBeDefined();
 			expect(post).toBeInstanceOf(Post);
-		}
+		});
+
+		it("should create one post with data", async () => {
+			const post = await postFactory.createOne({
+				title: "Test Post",
+				content: "This is a test post.",
+			});
+
+			expect(post.id).toBeDefined();
+			expect(post.title).toBe("Test Post");
+			expect(post.content).toBe("This is a test post.");
+		});
+	});
+
+	describe("createMany", () => {
+		it("should create many posts", async () => {
+			const posts = await postFactory.createMany(5);
+
+			expect(posts).toHaveLength(5);
+
+			for (const post of posts) {
+				expect(post.id).toBeDefined();
+				expect(post).toBeInstanceOf(Post);
+			}
+		});
+
+		it("should create many posts with data", async () => {
+			const postsData = [
+				{
+					title: "Test Post 1",
+					content: "This is a test post.",
+				},
+				{
+					title: "Test Post 2",
+					content: "This is another test post.",
+				},
+				{
+					title: "Test Post 3",
+					content: "This is yet another test post.",
+				},
+				{
+					title: "Test Post 4",
+					content: "This is a test post.",
+				},
+				{
+					title: "Test Post 5",
+					content: "This is another test post.",
+				},
+			];
+
+			const posts = await postFactory.createMany(5, postsData);
+
+			expect(posts).toHaveLength(5);
+
+			for (let i = 0; i < 5; i++) {
+				const post = posts[i];
+				const postData = postsData[i];
+
+				expect(post.id).toBeDefined();
+				expect(post.title).toBe(postData.title);
+				expect(post.content).toBe(postData.content);
+			}
+		});
 	});
 });
---
+++ test/typeorm-populate.spec.ts
@@ -0,0 +1,125 @@
+import { DataSource } from "typeorm";
+import { afterEach, beforeEach, describe, expect, it } from "bun:test";
+
+import { factories } from "./factories";
+import { TypeormPopulate } from "../src/typeorm-populate";
+
+import { User } from "./entities/user.entity";
+import { Post } from "./entities/post.entity";
+
+describe("TypeormPopulate", () => {
+	let dataSource: DataSource;
+
+	let typeormPopulate: TypeormPopulate;
+
+	beforeEach(async () => {
+		dataSource = new DataSource({
+			type: "sqlite",
+			database: ":memory:",
+			synchronize: true,
+			entities: [User, Post],
+		});
+
+		typeormPopulate = new TypeormPopulate({
+			dataSource,
+			factories,
+		});
+	});
+
+	it("should be defined", () => {
+		expect(typeormPopulate).toBeDefined();
+	});
+
+	afterEach(async () => {
+		if (typeormPopulate.isInitialized()) {
+			await typeormPopulate.destroy();
+		}
+	});
+
+	describe("initialize", () => {
+		it("should initialize the data source", async () => {
+			await typeormPopulate.initialize();
+
+			expect(typeormPopulate.isInitialized()).toBe(true);
+		});
+
+		it("should throw an error if the data source is already initialized", async () => {
+			await typeormPopulate.initialize();
+
+			await expect(typeormPopulate.initialize()).rejects.toThrow(
+				'Cannot create a "default" connection because connection to the database already established.',
+			);
+		});
+	});
+
+	describe("isInitialized", () => {
+		it("should return false if the data source is not initialized", () => {
+			expect(typeormPopulate.isInitialized()).toBe(false);
+		});
+
+		it("should return true if the data source is initialized", async () => {
+			await typeormPopulate.initialize();
+
+			expect(typeormPopulate.isInitialized()).toBe(true);
+		});
+	});
+
+	describe("destroy", () => {
+		it("should destroy the data source", async () => {
+			await typeormPopulate.initialize();
+			await typeormPopulate.destroy();
+
+			expect(typeormPopulate.isInitialized()).toBe(false);
+		});
+
+		it("should throw an error if the data source is not initialized", async () => {
+			await expect(typeormPopulate.destroy()).rejects.toThrow(
+				'Cannot execute operation on "default" connection because connection is not yet established.',
+			);
+		});
+	});
+
+	describe("seed", () => {
+		it("should seed the database with a specific number of records", async () => {
+			await typeormPopulate.initialize();
+			const res = await typeormPopulate.seed("user", 5);
+
+			expect(res).toHaveLength(5);
+		});
+
+		it("should throw an error if the factory does not exist", async () => {
+			await typeormPopulate.initialize();
+
+			await expect(typeormPopulate.seed("nonExistent", 5)).rejects.toThrow(
+				"Factory nonExistent not found",
+			);
+		});
+	});
+
+	describe("seedAll", () => {
+		it("should seed the database with multiple factories", async () => {
+			await typeormPopulate.initialize();
+			await typeormPopulate.seedAll([
+				{ name: "user", amount: 5 },
+				{ name: "post", amount: 10 },
+			]);
+
+			const users = await dataSource.getRepository(User).find();
+			const posts = await dataSource.getRepository(Post).find();
+
+			expect(users).toHaveLength(5);
+			expect(posts).toHaveLength(10);
+		});
+
+		it("should throw an error if the factory does not exist", async () => {
+			await typeormPopulate.initialize();
+
+			await expect(
+				typeormPopulate.seedAll([
+					{ name: "user", amount: 5 },
+					{ name: "nonExistent", amount: 10 },
+				]),
+			).rejects.toThrow("Factory nonExistent not found");
+		});
+	});
+});
