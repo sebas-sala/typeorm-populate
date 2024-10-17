d1932694ee1cd330f0cc134806639283849855d8
sebas-sala <sebastian_orko@hotmail.com>
2024-10-16 20:19:12 -0600

    Add formatters utility functions for text formatting and input validation

---
+++ src/utils/formatters.js
@@ -0,0 +1,15 @@
+function formatText(text) {
+  return text
+    .trim()
+    .replace(/[^a-zA-Z]/g, "")
+    .toLowerCase();
+}
+
+function isValidInput(input) {
+  return /^[a-zA-Z]+$/.test(input);
+}
+
+module.exports = {
+  formatText,
+  isValidInput,
+};
