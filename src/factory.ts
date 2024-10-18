68de9486e70ee48dddd7ba7ce2044ab179ca443b
sebas-sala <sebastian_orko@hotmail.com>
2024-10-17 22:59:14 -0600

    Refactor Factory class to support before and after create hooks

--- src/factory.ts
+++ src/factory.ts
@@ -2,13 +2,24 @@ import type { DataSource, ObjectLiteral, Repository } from "typeorm";
 
 export abstract class Factory<Entity extends ObjectLiteral> {
 	protected repository: Repository<Entity>;
+	private afterCreateHooks: Array<(entity: Entity) => Promise<void> | void> =
+		[];
+	private beforeCreateHooks: Array<
+		(entity: Partial<Entity>) => Promise<void> | void
+	> = [];
 
 	constructor(dataSource: DataSource, entity: new () => Entity) {
 		this.repository = dataSource.getRepository(entity);
 	}
 
-	protected defaultData(): Partial<Entity> {
-		return {};
+	protected abstract defaultData(): Partial<Entity>;
+
+	protected afterCreate(hook: (entity: Entity) => Promise<void>) {
+		this.afterCreateHooks.push(hook);
+	}
+
+	protected beforeCreate(hook: (entity: Partial<Entity>) => Promise<void>) {
+		this.beforeCreateHooks.push(hook);
 	}
 
 	async createOne(data?: Partial<Entity>): Promise<Entity> {
@@ -17,8 +28,18 @@ export abstract class Factory<Entity extends ObjectLiteral> {
 			...data,
 		} as Entity;
 
+		for (const hook of this.beforeCreateHooks) {
+			await hook(entityData);
+		}
+
 		const entity = this.repository.create(entityData);
-		return this.repository.save(entity);
+		const savedEntity = await this.repository.save(entity);
+
+		for (const hook of this.afterCreateHooks) {
+			await hook(savedEntity);
+		}
+
+		return savedEntity;
 	}
 
 	async createMany(
